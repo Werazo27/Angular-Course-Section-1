7aed6845ae0589345b1011dc61755815a76f89b7
Werazo27 <jlpriest27@gmail.com>
2025-01-01 23:56:49 -0500

    Lesson 16

--- src/app/model/course.ts
+++ src/app/model/course.ts
@@ -1,10 +1,9 @@
-
-
 export interface Course {
-    id:number;
-    description:string;
-    iconUrl: string;
-    longDescription: string;
-    category:string;
-    lessonsCount:number;
+  id: number;
+  description: string;
+  iconUrl: string;
+  altText: string;
+  longDescription: string;
+  category: string;
+  lessonsCount: number;
 }
