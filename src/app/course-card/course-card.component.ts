6cc036bfe55ebbbe1b35367efb7031fef0e5389f
Werazo27 <jlpriest27@gmail.com>
2025-01-03 00:47:51 -0500

    Removed unnecessary import

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -4,7 +4,7 @@ import { NgIf } from "@angular/common";
 
 @Component({
   selector: "course-card",
-  imports: [NgIf],
+  imports: [],
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",
 })
