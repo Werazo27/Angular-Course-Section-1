3f46052074cfa324c82c10be5b8aa61f91ae041b
Werazo27 <jlpriest27@gmail.com>
2025-01-03 19:48:36 -0500

    Lesson 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,12 @@ export class AppComponent {
   //   return course.id;
   //   // For when you need to track multiple properties
   // }
+
+  cardStyles(course: Course) {
+    if (course.category === "INTERMEDIATE") {
+      return { "margin-left": "-30%" };
+    } else if (course.category === "ADVANCED") {
+      return { "margin-right": "-30%" };
+    }
+  }
 }
